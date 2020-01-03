7714be5c0e193436d04008fdfefa9601af2e0830
Jefferson M Souza <jefferson.msouza86@gmail.com>
2020-01-02 22:34:06 -0300

    working with queries

--- src/datomic_explore/core.clj
+++ src/datomic_explore/core.clj
@@ -1,8 +1,31 @@
 (ns datomic-explore.core
   (:require [datomic-explore.db :as db]
+            [datomic.api :as d]
             [datomic-explore.model :as model]))
 
+(db/delete-database!)
+
+(db/create-database!)
 
 (let [conn (db/get-connection)]
   (db/create-schema! conn)
-  (db/add-product! conn [(model/new-product "Computer" "/computer" 3200.33M)]))
+  (db/add-product! conn [(model/new-product "Computer" "/computer" 3200.33M)])
+  (db/add-product! conn [(model/new-product "Test" "/test" 233.2M)]))
+
+(def db (d/db (db/get-connection)))
+(def db-old (d/as-of (d/db (db/get-connection)) #inst "2019-12-23T22:11:11.100"))
+
+(db/list-products db)
+
+(db/list-products-by-slug db "/computer")
+
+(db/list-slugs db)
+
+(db/list-prices db)
+
+(db/find-product-by-price db 500)
+
+(let [conn (db/get-connection)]
+  ;(db/create-schema! conn)
+  (d/transact conn [[:db/add 17592186045418 :product/keyword "computer"]
+                    [:db/add 17592186045420 :product/keyword "desktop"]]))
--- src/datomic_explore/db.clj
+++ src/datomic_explore/db.clj
@@ -24,7 +24,11 @@
              {:db/ident       :product/price
               :db/valueType   :db.type/bigdec
               :db/cardinality :db.cardinality/one
-              :db/doc         "Product's cardinality"}])
+              :db/doc         "Product's cardinality"}
+             {:db/ident       :product/keyword
+              :db/valueType   :db.type/string
+              :db/cardinality :db.cardinality/many
+              :db/doc         "Product's keyword"}])
 
 
 (defn create-schema! [conn]
@@ -32,3 +36,38 @@
 
 (defn add-product! [conn product]
   (d/transact conn product))
+
+;(defn list-products [db]
+;  (d/q '[:find (pull ?entity [:product/name :product/price :product/slug])
+;         :where [?entity :product/name]] db))
+
+(defn list-products [db]
+  (d/q '[:find (pull ?entity [*])
+         :where [?entity :product/name]] db))
+
+(defn list-products-by-slug [db slug]
+  (d/q '[:find ?entity
+         :in $ ?slug-param
+         :where [?entity :product/slug ?slug-param]]
+       db slug))
+
+(defn list-slugs [db]
+  (d/q '[:find ?slug
+         :where [_ :product/slug ?slug]]
+       db))
+
+(defn list-prices [db]
+  (d/q '[:find ?name ?price
+         ;:keys product/name product/price
+         :where [?p :product/price ?price]
+                [?p :product/name ?name]]
+       db))
+
+(defn find-product-by-price [db minimum-price]
+  (d/q '[:find ?name ?price
+         ;:keys product/name product/price
+         :in $ ?min-price-param
+         :where [?p :product/price ?price]
+                [(>= ?price ?min-price-param)]
+                [?p :product/name ?name]]
+       db minimum-price))
